b4fb076941b977168982ea743937c65a9d1760aa
samuelefaraoni <samuele.faraoni.stud@ispascalcomandini.it>
2026-05-26 21:22:43 +0200

    Added Library.Console

---
+++ src/Library.Console/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Console
+{
+    public class Controller
+    {
+        public string ShowMenu()
+        {
+            return ("1) Create and add book to library./n2) Search book by title./n3) Search book by author./n 4) Return book count./n5) Exit program.");
+        }
+    }
+}
---
+++ src/Library.Console/Program.cs
@@ -0,0 +1,53 @@
+﻿using Library.Console;
+using System.Runtime.CompilerServices;
+
+bool executionStatus = true;
+Controller controller = new Controller();
+Library library = new Library("Biblioteca Malatestiana", "Piazza Maurizio Bufalini 1. 47521 Cesena(FC)", 9, 0, 19, 0);
+
+while (executionStatus == true)
+{
+    controller.ShowMenu();
+    string choice = Console.ReadLine();
+
+    switch(choice)
+    {
+        case "1":
+            Console.WriteLine("Insert title");
+            string title = Console.ReadLine();
+            Console.WriteLine("Insert author");
+            string author = Console.ReadLine();
+            Console.WriteLine("Insert publication year");
+            Int32.TryParse(Console.ReadLine(), out int result);
+            int publicationYear = result;
+            Console.WriteLine("Insert publisher");
+            string publisher = Console.ReadLine();
+            Console.WriteLine("Insertthe number of pages");
+            Int32.TryParse(Console.ReadLine(), out int result2);
+            int pages = result2;
+
+            Book book = new Book(title, author, publicationYear, publisher, pages);
+
+            library.BookList.Add(book);
+            break;
+        case "2":
+            Console.WriteLine("Insert book title");
+            string title = Console.ReadLine();
+            library.SearchBook(title);
+            break;
+        case "3":
+            Console.WriteLine("Insert book author");
+            string author = Console.ReadLine();
+            library.GetBooksByAuthor(author);
+            break;
+        case "4":
+            library.GetBooksCount();
+            break;
+        case: "5":
+            executionStatus = false;
+            break;
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+}
